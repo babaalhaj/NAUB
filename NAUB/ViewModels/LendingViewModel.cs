410b55e9380b1e59b2d166fd3078c68af1bec995
“Iliyasu <ilyas.alkali@gmail.com>
2020-01-16 00:27:24 +0100

    Added Days Remaining and Default days remaining to LendingViewModel and Lending Visew

--- NAUB/ViewModels/LendingViewModel.cs
+++ NAUB/ViewModels/LendingViewModel.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using NAUB.Models;
+﻿using NAUB.Models;
+using System;
 
 namespace NAUB.ViewModels
-{
-    public class LendingViewModel
     {
+    public class LendingViewModel
+        {
         public int Id { get; set; }
         public Borrow Borrow { get; set; }
         public DateTime ExpectedReturnDate { get; set; }
-        public string BorrowType
-        {
-            get { return Borrow.BorrowerType == "1" ? "Staff" : "Student"; }
+        public string BorrowType => Borrow.BorrowerType == "1" ? "Staff" : "Student";
+        public int DefaultDaysRemaining() => (int)(ExpectedReturnDate.Date - Borrow.BorrowDate.Date).TotalDays;
+        public int DaysRemaining() => (int)(ExpectedReturnDate.Date - DateTime.Now.Date).TotalDays ;
+
+
         }
     }
-}
