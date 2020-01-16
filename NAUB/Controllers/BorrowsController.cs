70e232ae4aeccdbefa59603c8950b913c4c100f9
“Iliyasu <ilyas.alkali@gmail.com>
2020-01-16 08:57:15 +0100

    Completed extending booking

--- NAUB/Controllers/BorrowsController.cs
+++ NAUB/Controllers/BorrowsController.cs
@@ -1,65 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using NAUB.Models;
+﻿using NAUB.Models;
 using NAUB.ViewModels;
+using System;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace NAUB.Controllers
-{
-    public class BorrowsController : Controller
     {
+    public class BorrowsController : Controller
+        {
         private readonly ApplicationDbContext _context;
 
         public BorrowsController()
-        {
-                _context = new ApplicationDbContext();
-        }
+            {
+            _context = new ApplicationDbContext();
+            }
 
         protected override void Dispose(bool disposing)
-        {
-            if(disposing)
+            {
+            if (disposing)
                 _context.Dispose();
-        }
+            }
 
         // TODO: List of Borrowers
         // GET: /Borrows/
         public ActionResult Index()
-        {
+            {
             ViewBag.Borrowers = _context.Borrows.Count();
             var processBorrowers = new ProcessLendingViewModel(_context);
             return View(processBorrowers.GetLendingDetails());
-        }
+            }
 
         //
         public ActionResult AddRecord(short getCount)
-        {
+            {
             var getSettings = _context.Settings.FirstOrDefault();
 
             if (getSettings != null && getCount > getSettings.MaximumNumberOfBooksPerBorrow)
-                return Content("The maximum number of books exceeded. The maximum per individual is " 
+                return Content("The maximum number of books exceeded. The maximum per individual is "
                                + getSettings.MaximumNumberOfBooksPerBorrow
-                               +" books");
+                               + " books");
 
-            
+
             var borrowViewModel = new BorrowViewModel(getCount)
-            {
+                {
                 BorrowTypes = _context.BorrowTypes.ToList()
-            };
+                };
 
             return View(borrowViewModel);
-        }
+            }
 
         public ActionResult NumberOfBooks()
-        {
+            {
             return View();
-        }
+            }
 
         // TODO: GET: /Borrows/SaveRecord
         [HttpPost]
         public ActionResult AddRecord(BorrowViewModel borrowViewModel)
-        {
+            {
             // TODO: Implement save borrow validation
             //if (!ModelState.IsValid)
             //    return View("AddRecord", borrowViewModel);
@@ -86,7 +84,39 @@ namespace NAUB.Controllers
 
             _context.SaveChanges();
 
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
+            }
+
+        // GET:/Borrows/Extend
+        public ActionResult Extend(string borrowerId)
+        {
+            var borrow = _context.Borrows.SingleOrDefault(b => b.BorrowerId == borrowerId);
+            //var borrow = _context.Borrows.Single(l => l.Id == id);
+            var extend = new ExtendViewModel
+            {
+                BorrowerId = borrow.BorrowerId, BorrowerType = borrow.BorrowerType,
+                Name = borrow.Name, NumberOfDays = 0
+            };
+
+            return View(extend);
         }
-	}
-}
+
+        [HttpPost]
+        public ActionResult Extend(ExtendViewModel model)
+        {
+            // Arrange
+            var borrow = _context.Borrows.Single(b => b.BorrowerId == model.BorrowerId);
+            borrow.BorrowDate = borrow.BorrowDate.AddDays(model.NumberOfDays);
+
+            // Save extension date to database
+            _context.SaveChanges();
+
+            // Initialize the Lending List
+            ViewBag.Borrowers = _context.Borrows.Count();
+            var processBorrowers = new ProcessLendingViewModel(_context);
+
+            return View("Index", processBorrowers.GetLendingDetails());
+        }
+
+        }
+    }
---
+++ NAUB/ViewModels/ExtendViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NAUB.ViewModels
+    {
+    public class ExtendViewModel
+        {
+            public string BorrowerType { get; set; }
+            public string BorrowerId { get; set; }
+            public string Name { get; set; }
+            public string Status() => (BorrowerType == "1") ? "Staff" : "Student";
+            public int NumberOfDays { get; set; }
+        }
+    }
